f9ad46da2f09963dd180e033916d08964967cb07
cohlar <cohlar@gmail.com>
2020-01-28 13:07:14 +0200

    updated server url for example file

---
+++ example/example.py
@@ -0,0 +1,14 @@
+import requests
+
+
+url = 'https://chatbot-discovery.herokuapp.com/'
+api = 'api/discovery'
+endpoint = {'endpoint': 'ENTER YOUR ENDPOINT URL HERE'}
+
+
+post = requests.post(url + api, json=endpoint)
+print(post.text)
+
+
+get = requests.get(url + api)
+print(get.text)
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, abort, jsonify
+import os
 
 
 app = Flask(__name__)
